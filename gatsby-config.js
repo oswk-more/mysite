d02fc68e0b5af744ab9c37f880854447710b9b51
oswk-more <oswk.more+dev@gmail.com>
2022-11-14 14:35:49 +0900

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+}
